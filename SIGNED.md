e6cf426e67d51810cd01c4740d4d45f91589d5fb
Chris Collins <collins.christopher@gmail.com>
2015-01-29 17:06:10 -0500

    adding support for setting the php-fpm thread numbers

--- SIGNED.md
+++ SIGNED.md
@@ -3,9 +3,9 @@
 -----BEGIN PGP SIGNATURE-----
 Version: GnuPG v1
 
-iEYEABECAAYFAlSsV4cACgkQte6EFif3vzeLjwCgzL+deuAICV48NIPxR6SefNOc
-GZUAoJanKtfaQI+Y8fJnGCuADO/jBhVY
-=jB7U
+iEYEABECAAYFAlTKrpUACgkQte6EFif3vzd9xwCfbpl6IHHwHTSY+NEh5WW536ku
+dG0AoOruYL6bCunuyCUHOTDTyKOwNmI0
+=HENy
 -----END PGP SIGNATURE-----
 
 ```
@@ -23,7 +23,7 @@ size   exec  file               contents
 35121          LICENSE          e1c0ad728983d8a57335e52cf1064f1affd1d454173d8cebd3ed8b4a72b48704
 1735           README.md        8d4c94c21dcef724fd6bd6d51d12ddac52049fc00987ff35591be695b9eea612
 858    x       config-ssmtp.sh  7c6751090bbf595682ceba3dc8151ceead2d3de16b8a280460af74a7a574af33
-252    x       run-phpfpm.sh    0aba66ef402f514363a2ffc57d3379f06e4512c1837f9f5facf3a6f793d69134
+1245   x       run-phpfpm.sh    9bb56372a90ddccca2c8fe80b60830e0f985a4e1463b76550ea641731f22bd24
 ```
 
 #### Ignore
--- run-phpfpm.sh
+++ run-phpfpm.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 
-/config-ssmtp.sh
+LOGDIR='/var/log/php-fpm'
+THREADYAML='/conf/threads.yaml'
+HOSTCONF='www.conf'
 
-if [[ ! -d /var/log/php-fpm ]] ; then
-  mkdir -p /var/log/php-fpm
+f_set_threads () {
+  if [[ $1 == '--default' ]] ; then
+    MAX_CHILD='20'
+    MIN_SPARE='5'
+    MAX_SPARE='10'
+    MAX_REQUESTS='500'
+  else
+    MAX_CHILD="$(awk '/fpm_fpm_maxchild/ {print $2}' $THREADYML)"
+    MIN_SPARE="$(awk '/fpm_fpm_maxchild/ {print $2}' $THREADYML)"
+    MAX_SPARE="$(awk '/fpm_fpm_maxchild/ {print $2}' $THREADYML)"
+    MAX_REQUESTS="$(awk '/fpm_fpm_maxchild/ {print $2}' $THREADYML)"
+  fi
+  sed -i "s/.*pm.max_children\ =\ .*/pm.max_children = $MAX_CHILD/" $HOSTCONF
+  sed -i "s/.*pm.min_spare_servers\ =\ .*/pm.min_spare_servers = $MIN_SPARE/" $HOSTCONF
+  sed -i "s/.*pm.max_spare_servers\ =\ .*/pm.max_spare_servers = $MAX_SPARE/" $HOSTCONF
+  sed -i "s/.*pm.max_requests\ =\ .*/pm.max_requests = $MAX_REQUESTS/" $HOSTCONF
+}
+
+# Make the log dirs (in case we mount volumes from elsewhere)
+if [[ ! -d $LOGDIR ]] ; then
+  mkdir -p $LOGDIR
 fi
-if [[ ! -f /var/log/php-fpm/error.log ]] ; then
-  touch /var/log/php-fpm/error.log
+if [[ ! -f $LOGDIR/error.log ]] ; then
+  touch $LOGDIR/error.log
 fi
 
-chown -R apache.root /var/log/php-fpm
+chown -R apache.root $LOGDIR
+
+# Setup the thread count
+if [[ -f $THREADYAML ]] ; then
+  f_set_threads
+else
+  f_set_threads --default
+fi
+
+# Run the SSMTP setup script
+/bin/bash /config-ssmtp.sh
 
 exec /usr/sbin/php-fpm -F
